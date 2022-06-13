3897fb586016ef4694c65aff80b8872e27a86bc3
SSK137 <saurabhkodam@gmail.com>
2022-06-13 14:29:40 +0530

    Added JS Function to validate Name

--- Form.js
+++ Form.js
@@ -1,1 +1,17 @@
-console.log("Welcome to Validation HTML using JS ")
+const salary = document.querySelector('#salary');
+        const output = document.querySelector('.salary-output');
+        output.textContent = salary.value;
+        salary.addEventListener('input', function () {
+            output.textContent = salary.value;
+        });
+
+        
+// Validation For Name
+const text= document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input',function(){
+    let nameRgx = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRgx.test(text.value))
+    textError.textContent= "";
+    else textError.textContent = "Name Is Incorrect";
+});
